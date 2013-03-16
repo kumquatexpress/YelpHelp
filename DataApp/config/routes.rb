c4976b5eea4b03bf3abe3f3278f0cd093a58b08d
Boyang Niu <boyang.niu@gmail.com>
2013-03-16 01:31:13 -0400

    roots to admin dashboard

--- DataApp/config/routes.rb
+++ DataApp/config/routes.rb
@@ -56,7 +56,7 @@ DataApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'admin/dashboard#index' 
+  root :to => 'rails_admin/main#dashboard' 
  
   # See how all your routes lay out with "rake routes"
 
