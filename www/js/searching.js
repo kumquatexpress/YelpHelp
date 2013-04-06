b60db55aa4980ba05762f7409a4d3a6066ccaf7e
Patrick Hulce <patrick.hulce@gmail.com>
2013-04-06 19:40:42 -0400

    Testing namespacing with jQuery, javascript, and multiple self-executing anonymous functions

--- www/js/searching.js
+++ www/js/searching.js
@@ -1,10 +1,16 @@
+(function(test) {
+	test.my_func = function() {
+		alert("Yo it works!");
+	}
+}(window.test));
+
 (function(search, $, undefined) {
 	var searchURL = "./testdata/restaurants_by_dish.json";
 	search.findRestaurantsByDish = function(dish) {
 		var data = {"dish" : $("#search_box").val()};
-		alert("testing");
+		test.my_func();
 		$.post(searchURL,data,function(response){
-			alert(response);
+			alert("post successful")
 		});
 	};
 }(window.search = window.search || {},jQuery));
