1a8b9e610b94ea85abbb50c3099cb2fc9be68c2f
Patrick Hulce <patrick.hulce@gmail.com>
2013-04-07 12:56:35 -0400

    Got ajax working, began styling results

--- www/js/searching.js
+++ www/js/searching.js
@@ -1,16 +1,29 @@
-(function(test) {
-	test.my_func = function() {
-		alert("Yo it works!");
-	}
-}(window.test));
-
 (function(search, $, undefined) {
-	var searchURL = "./testdata/restaurants_by_dish.json";
+	var searchURL = "http://localhost:8080/testdata/restaurants_by_dish.json";
 	search.findRestaurantsByDish = function(dish) {
 		var data = {"dish" : $("#search_box").val()};
-		test.my_func();
-		$.post(searchURL,data,function(response){
-			alert("post successful")
+		$.ajax({
+			url : searchURL,
+			cache : false,
+		}).done(function(raw_data) {
+			var restaurants = $.parseJSON(raw_data);
+			$('.results').html('');
+			for(var i=0;i < restaurants.results.length;i++) {
+				var r = restaurants.results[i];
+				var rating = "<div class='quality'><img src='img/thumbs-";
+				if(r.rating > 5.0) {
+					rating += "up";
+				}
+				else {
+					rating += "down";
+				}
+				rating += ".png' /></div>";
+				var name = "<div class='name'>"+r.name+"</div>";
+				var address = "<div class='address'>"+r.address+"</div>";
+				var info = "<div class='info'>"+name+address+"</div>"
+				var r_str = "<div class='result'>"+rating+info+"</div>";
+				$('.results').append(r_str);
+			}
 		});
 	};
 }(window.search = window.search || {},jQuery));
