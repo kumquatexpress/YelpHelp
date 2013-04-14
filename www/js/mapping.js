aa65bcd81d32b414b4e23ee66b4233201bc0fad0
Patrick Hulce <patrick.hulce@gmail.com>
2013-04-14 14:29:34 -0400

    Finished adding the path plotting, added listening library, remainder of UI controls.

--- www/js/mapping.js
+++ www/js/mapping.js
@@ -1,7 +1,11 @@
 (function(mapping, $, undefined) {
     mapping.map = null;
     var geocoder = new google.maps.Geocoder();
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = null;
+    var geocodes = {};
     var markers = [];
+    var path = [];
     var addMarker = function(the_title, latlng) {
         var marker = new google.maps.Marker({
             position : latlng,
@@ -19,14 +23,20 @@
         };
         mapping.map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+        directionsDisplay = new google.maps.DirectionsRenderer({map : mapping.map});
     };
 
     mapping.getCoordsFromAddress = function (address, callback) {
+        if (geocodes[address] != undefined) {
+            callback(geocodes[address]);
+            return;
+        }
         geocoder.geocode( { 'address': address}, function(results, status) {
             console.log("Got geocoding result");
             if (status == google.maps.GeocoderStatus.OK) {
                 var loc = results[0].geometry.location;
                 console.log(loc.toString());
+                geocodes[address] = loc;
                 callback(loc);
             } else {
                 console.log("Unable to geocode " + address + " because " + status);
@@ -36,7 +46,7 @@
 
     mapping.placeMarker = function (name, address) {
 
-        callback = function(latlng) {
+        var callback = function(latlng) {
             addMarker(name,latlng);
         };
         mapping.getCoordsFromAddress(address,callback);
@@ -49,5 +59,45 @@
             }
             markers.length = 0;
         }
+    };
+
+    mapping.placeRoute = function(address_list){
+        path.length = 0;
+        var callback = function(loc) {
+            path.push(loc)
+        };
+        for(var i=0;i<address_list.length;i++) {
+            mapping.getCoordsFromAddress(address_list[i],callback);
+        }
+        listening.showLoading();
+        setTimeout(function() {
+            if (path.length < 2) {
+                return;
+            }
+            var wps = [];
+            var orig = path[0];
+            var dest = path[1];
+            if (path.length == 3) {
+                wps.push({location:path[1]});
+                dest = path[2];
+            }
+            var request = {
+                origin: orig,
+                destination: dest,
+                waypoints: wps,
+                travelMode: google.maps.DirectionsTravelMode.DRIVING
+            };
+            directionsService.route(request, function(response,status) {
+                if (response.status ==  "OK") {
+                    directionsDisplay.setDirections(response);
+                }
+                else {
+                    alert("Failed");
+                    console.log(response);
+                    console.log(status);
+                }
+            });
+            listening.hideLoading();
+        },2000);
     };
 }(window.mapping = window.mapping || {},jQuery));
