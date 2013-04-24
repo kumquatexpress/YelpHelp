ba9df0fc496d5612b95c1ab23df4634940da360a
jkodner05 <jkodner05@yahoo.com>
2013-04-23 21:08:25 -0400

    better foodmap for philly

--- nlp_material/get_foods.py
+++ nlp_material/get_foods.py
@@ -3,26 +3,42 @@ import urllib2
 from pprint import pprint
 import nltk
 
-#jdata = urllib2.urlopen('http://174.129.210.6/reviews/all_by_business/100.json').read()
+nns = set([])
+for index in range(0, 100):
+    print index
+    try:
+        jdatafile = urllib2.urlopen('http://174.129.210.6/reviews/close_to_philly/'+str(index*100)+'/'+str((index+1)*100)+'.json').read()
+    except:
+        print "quitting"
+        break
+
 #print jdata
 
 
-jdata = open('jdata.json')
-data = json.load(jdata)
+#jdata = open('jdata.json')
+    jdata = open('jdata.json',"w")
+    jdata.write(jdatafile)
+    jdata.close()
+    jdata = open('jdata.json')
+    data = json.load(jdata)
+    print "read json file"
 #pprint(data)
 
-nns = set([])
-for biz in data:
-    for review in biz["reviews"]:
-        text = review["text"]
-        tokenized = nltk.word_tokenize(text.lower().strip())
-        tagged = nltk.pos_tag(tokenized)
-        for pair in tagged:
-            if pair[-1] == "NN":
-                nns.add(pair[0])
+
+    for biz in data:
+        for review in biz["reviews"]:
+            text = review["text"]
+            tokenized = nltk.word_tokenize(text.lower().strip())
+            tagged = nltk.pos_tag(tokenized)
+            for pair in tagged:
+                if pair[-1] == "NN":
+                    nns.add(pair[0])
+
+f = open("raw_foods.txta","w")
 for word in nns:
     try:
         print word
+        f.write(word + "\n")
     except:
         pass
 
