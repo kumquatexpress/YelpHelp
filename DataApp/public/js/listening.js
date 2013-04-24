a0e0c92e5484e0b674bcaffa31dcf2936b9d61ef
Patrick Hulce <patrick.hulce@gmail.com>
2013-04-23 21:41:39 -0400

    Added randomness and other features

--- DataApp/public/js/listening.js
+++ DataApp/public/js/listening.js
@@ -3,7 +3,7 @@
     listening.init = function() {
         listening.plotPath();
         $("#search_box").keydown(function(e) {
-            if(e.keyCode == 13) {
+            if (e.keyCode == 13) {
                 searching.findRestaurants($(this).val());
             }
         });
@@ -11,27 +11,51 @@
 
     listening.plotPath = function() {
         $("#plot-path").click(function() {
-            var vals =  [$("#stop-1").val(), $("#stop-2").val(), $("#stop-3").val()];
+            var vals = [$("#stop-1").val(), $("#stop-2").val(), $("#stop-3").val()];
             var addrs = [];
-            for(var i=0;i<vals.length;i++) {
+            for (var i = 0; i < vals.length; i++) {
                 if (vals[i].length > 3) {
                     addrs.push(vals[i]);
                 }
             }
-            if(addrs.length > 1) {
-                mapping.placeRoute(addrs);
-            }
-            else {
+            if (addrs.length > 1) {
+                mapping.placeRouteByAddress(addrs);
+            } else {
                 alert("Not enough stops for a route!");
             }
         });
     };
 
-    listening.showLoading = function() {
-        $("#loading").show();
+    listening.plotMeals = function() {
+        $("#plot-meals").click(function() {
+            var vals = [$("#dish-1").val(), $("#dish-2").val(), $("#dish-3").val()];
+            var dishes = [];
+            for (var i = 0; i < vals.length; i++) {
+                if (vals[i].length > 3) {
+                    dishes.push(vals[i]);
+                }
+            }
+            if (dishes.length > 0) {
+                searching.findRestaurantsRoute(dishes);
+            } else {
+                alert("Not enough stops for a route!");
+            }
+        });
     };
 
-    listening.hideLoading = function() {
-        $("#loading").hide();
+    listening.plotRandom = function() {
+        $("#plot-random").click(function() {
+            var val = $("#place").val();
+            if (val.length > 0) {
+                var callback = function(pos) {
+                    mapping.map.setCenter(pos);
+                    searching.findRestaurants(undefined);
+                }
+                mapping.getCoordsFromAddress(val, callback);
+            } else {
+                alert("Please enter a place!");
+            }
+        });
     };
-}(window.listening = window.listening || {},jQuery))
+
+}(window.listening = window.listening || {}, jQuery))
--- DataApp/public/js/searching.js
+++ DataApp/public/js/searching.js
@@ -1,5 +1,7 @@
 (function(searching, $, undefined) {
 	var restaurantURL = document.location.href + "businesses/list_restaurants";
+	var restaurantPathURL = document.location.href + "businesses/list_restaurants";
+
 	searching.findRestaurants = function(dish) {
 		var coords = mapping.map.getCenter();
 		var data_vars = {"dish" : dish, "lat" : coords.lat(),"long" : coords.lng()};
@@ -8,12 +10,12 @@
 			data : data_vars,
 			cache : false
 		}).done(function(raw_data) {
-			var restaurants = raw_data;
+			var restaurants = $.parseJSON(raw_data);
 			$('.results').html('');
 			mapping.clearMarkers();
 
-			for(var i=0;i < restaurants.results.length;i++) {
-				var r = restaurants.results[i];
+			for(var i=0;i < restaurants.length;i++) {
+				var r = restaurants[i];
 				var rating = "<div class='quality'><img src='img/thumbs-";
 				if(r.rating > 3.0) {
 					rating += "up";
@@ -27,8 +29,42 @@
 				var info = "<div class='info'>"+name+address+"</div>";
 				var r_str = "<div class='result'>"+rating+info+"</div>";
 				$('.results').append(r_str);
-				mapping.placeMarker(r.name,r.latlng);
+				mapping.placeMarker(r.name,new GLatLng(r.latlng[0],r.latlng[1]));
 			}
 		});
 	};
+
+	searching.findRestaurantsRoute = function(dishes) {
+		var coords = mapping.map.getCenter();
+		var data_vars = {"dishes" : dishes, "lat" : coords.lat(),"long" : coords.lng()};
+		$.ajax({
+			url : restaurantPathURL,
+			data : data_vars,
+			cache : false
+		}).done(function(raw_data) {
+			var restaurants = raw_data;
+			$('.results').html('');
+			mapping.clearMarkers();
+			var path = [];
+			for(var i=0;i < restaurants.length;i++) {
+				var r = restaurants[i];
+				var rating = "<div class='quality'><img src='img/thumbs-";
+				if(r.rating > 3.0) {
+					rating += "up";
+				}
+				else {
+					rating += "down";
+				}
+				rating += ".png' /></div>";
+				var name = "<div class='name'>"+r.name+"</div>";
+				var address = "<div class='address'>"+r.address+"</div>";
+				var info = "<div class='info'>"+name+address+"</div>";
+				var r_str = "<div class='result'>"+rating+info+"</div>";
+				$('.results').append(r_str);
+				path.push(new GLatLng(r.latlng[0],r.latlng[1]));
+			}
+			mapping.placeRoute(path);
+		});
+	};
+
 }(window.searching = window.searching || {},jQuery));
